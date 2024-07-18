33d2a8700bf87c30afb5b18ad62bd801348ed165
siwoo <k74415834@gmail.com>
2024-07-18 16:40:19 +0900

    init: init project

---
+++ src/main/java/com/project/apiDogs/ApiDogsApplication.java
@@ -0,0 +1,13 @@
+package com.project.apiDogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDogsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/apiDogs/common/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.project.apiDogs.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())//.formLogin(AbstractHttpConfigurer::disable) //TODO 테스트용 form login
+                .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .logout(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
